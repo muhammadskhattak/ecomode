83562565542065b696f79d74e71d5e48a23d5d2d
Muhammad Khattak <muhammadskhattak@gmail.com>
2018-12-20 22:46:55 +0000

    Made minor modifications for the banner

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../Stylesheets/App.css';
 import Home from '../Home/Home';
-import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Home />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Home />
+      </div>
     );
   }
 }
--- src/GlobalComponents/PathNotFound.js
+++ src/GlobalComponents/PathNotFound.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import EcomodeNav from './EcomodeNav';
 
 const pathNotFound = (props) => {
     return (
         <div>
+            <EcomodeNav />
             <h1>404: Page Not Found!</h1>
             <h2>You can navigate back to Project Ecomode by clicking the link below</h2>
             <a href="/Home">Back to the Home Page</a>
