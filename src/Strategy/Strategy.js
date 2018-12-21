2425ba72ebf0af349f44c2a73abe2300b722ee64
Muhammad Khattak <muhammadskhattak@gmail.com>
2018-12-21 01:27:15 +0000

    Deleted _redirects as that was the wrong location for the file. Added Navbar to both /tickets and /strategy

--- src/Strategy/Strategy.js
+++ src/Strategy/Strategy.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import EcomodeNav from '../GlobalComponents/EcomodeNav';
 
 const strategy = (props) => {
     return (
         <div>
+            <EcomodeNav />
             <p>This page contains information on our Business Strategy!</p>
         </div>
     );
--- src/Tickets/Tickets.js
+++ src/Tickets/Tickets.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import EcomodeNav from '../GlobalComponents/EcomodeNav';
 
 const tickets = (props) => {
     return (
         <div>
+            <EcomodeNav />
             <p>This is where you can get information on how to buy ticekts!</p>
         </div>
     );
